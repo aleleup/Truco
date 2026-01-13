997595a9bac6102e11916c4daf38410e6a3d12ed
Alejandro Leupold de Souza <aleupold@dappsfactory.io>
2026-01-13 20:43:10 -0300

    multiple games at a time -> check

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from classes.GameDesk import GameDesk
 from classes.ConnectionManager import ConnectionManager
 from constants.types import *
+from fastapi.middleware.cors import CORSMiddleware
+
 # from classes.TrucoDeck import TrucoDeck
 ############# AUX-FUNCTIONS #############
 async def send_players_status(game_desk:GameDesk, connection_manager: ConnectionManager):
@@ -16,11 +18,28 @@ async def send_players_status(game_desk:GameDesk, connection_manager: Connection
 if __name__ == "main":
 
     app = FastAPI()
+    app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+    desk_per_session: dict[int, GameDesk] = {}
+    public_manager_per_session: dict[int, ConnectionManager] = {}
+    private_manager_per_session: dict[int, ConnectionManager] = {}
+    host_per_session: dict[int, ConnectionManager] = {}
+
+    @app.get("/store-session-id/{session}")
+    async def store_session_id(session: int):
+        if session not in host_per_session:
+            host_per_session[session] = ConnectionManager(2)
+        return {"message": "session already stored"}
 
     ### KEEP CLIENTS IN LOBBY UNTIL TWO PLAYERS ARE CONNECTED (There's no need to use databases for this logic now)
-    host: ConnectionManager = ConnectionManager(2)
-    @app.websocket("/enter-lobby/{id}")
-    async def receive_player(websocket: WebSocket, id:str):
+    @app.websocket("/enter-lobby/{session}/{id}")
+    async def receive_player(websocket: WebSocket, id:str, session: int):
+        host: ConnectionManager = host_per_session[session]
         new_id: int = 1 if host.connections_amount() > 0 else 0
         print("CONNECTION NEW ID", new_id)
         await host.connect(new_id, websocket) # send them into the lobby and return their new id
@@ -28,6 +47,9 @@ if __name__ == "main":
         
         try:
             if host.connections_amount() == 2:
+                desk_per_session[session] = GameDesk()
+                public_manager_per_session[session] = ConnectionManager(2)
+                private_manager_per_session[session] = ConnectionManager(2)
                 await host.broadcast(json.dumps({"allow_access": True}))
 
                 # Give the OS time to flush frames --> AI HELP
@@ -43,48 +65,55 @@ if __name__ == "main":
             await host.shutdown()
 
 
-    public_data_manager: ConnectionManager = ConnectionManager(2)
-    @app.websocket("/public-view/{client_id}")
-    async def public_views(websocket: WebSocket, client_id: int):
+    @app.websocket("/public-view/{session}/{client_id}")
+    async def public_views(websocket: WebSocket, client_id: int, session: int):
+        public_data_manager: ConnectionManager = public_manager_per_session[session]
+        private_data_manager: ConnectionManager = private_manager_per_session[session]
         await public_data_manager.connect(client_id, websocket)
-        if players_middleware.connections_amount() == 2 and public_data_manager.connections_amount() == 2:
-            await brodcast_public_data(public_data_manager)
+        if private_data_manager.connections_amount() == 2 and public_data_manager.connections_amount() == 2:
+            await brodcast_public_data(public_data_manager, session)
         try:
             while True:
                 await websocket.receive_text()
                 
         except WebSocketDisconnect:
             await public_data_manager.shutdown()
-            restart_desk_and_middleware()
+            delete_desk_and_manager_at_session(session)
 
-    async def brodcast_public_data(connection_managger: ConnectionManager):
+    async def brodcast_public_data(connection_managger: ConnectionManager, session: int):
+        desk: GameDesk = desk_per_session[session]
+        private_data_manager: ConnectionManager = private_manager_per_session[session]
+
         general_data: PublicData = desk.get_general_view()
         await connection_managger.broadcast(json.dumps(general_data))
         
 
         if general_data["winner_id"] != -1:
-            return restart_desk_and_middleware()
+            return delete_desk_and_manager_at_session(session)
 
         if general_data["round_winner"] != -1:
             # STARTING DANGEROUS RECURSION
             await asyncio.sleep(3)
             desk.init_round()
-            await send_players_status(desk, players_middleware)
-            await brodcast_public_data(connection_managger) # NOW general_data["round_winner"] == -1
+            await send_players_status(desk, private_data_manager)
+            await brodcast_public_data(connection_managger, session) # NOW general_data["round_winner"] == -1
 
 
 
-    desk: GameDesk = GameDesk()
-    players_middleware: ConnectionManager = ConnectionManager(2)
-    @app.websocket("/playground/{id}")
-    async def start_game(websocket: WebSocket, id: int):
+
+    @app.websocket("/playground/{session}/{id}")
+    async def start_game(websocket: WebSocket, id: int, session: int):
+        desk: GameDesk = desk_per_session[session]
+        private_data_manager: ConnectionManager = private_manager_per_session[session]
+        public_data_manager: ConnectionManager = public_manager_per_session[session]
+
         print(f"{id} arrived")
-        await players_middleware.connect(int(id), websocket)
+        await private_data_manager.connect(int(id), websocket)
 
-        if players_middleware.connections_amount() == 2:
+        if private_data_manager.connections_amount() == 2:
             print("THERE ARE TWO")
             desk.init_round()
-            await send_players_status(desk, players_middleware)
+            await send_players_status(desk, private_data_manager)
         try:
             while True:  # keep this socket alive forever
                 raw = await websocket.receive_text()
@@ -94,14 +123,13 @@ if __name__ == "main":
                 
                 new_action = PlayersActions(**player_action)
                 desk.receive_players_action(id, new_action)
-                await send_players_status(desk, players_middleware)
-                await brodcast_public_data(public_data_manager)
+                await send_players_status(desk, private_data_manager)
+                await brodcast_public_data(public_data_manager, session)
         except WebSocketDisconnect:
-            await players_middleware.shutdown()
-            restart_desk_and_middleware()
+            await private_data_manager.shutdown()
+            delete_desk_and_manager_at_session(session)
 
-    def restart_desk_and_middleware():
-        global desk, players_middleware, public_data_manager
-        desk = GameDesk()
-        players_middleware = ConnectionManager(2)
-        public_data_manager = ConnectionManager(2)
+    def delete_desk_and_manager_at_session(session: int):
+        del desk_per_session[session]
+        del public_manager_per_session[session]
+        del private_manager_per_session[session]
